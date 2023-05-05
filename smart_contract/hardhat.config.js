59824d59f5efeebee27aff86c1df278cf9105871
Nonso <nonso4emma@yahoo.com>
2023-05-05 07:02:49 +0300

    First pR

---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,9 @@
+// https://eth-sepolia.g.alchemy.com/v2/Yu83Wqi9YFfxs3IKgZ8v2S0c8Uy75pvA
+
+// require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.18",
+};
